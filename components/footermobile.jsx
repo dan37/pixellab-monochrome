b376d859145cfdcdf61974c6038c8df0844e76ef
dan37 <gabriandanut@yahoo.com>
2022-07-10 12:51:04 +0300

    monochrome responsive update

--- components/footermobile.jsx
+++ components/footermobile.jsx
@@ -3,7 +3,7 @@
       <div className="footer-mobile">
         <div className="content-mobile container-mobile  d-block  d-md-none d-lg-none">
         <div className="footer-connect">
-          <div className="footer-newsletter-mobile">
+          <div className="footer-newsletter">
             <h1 className="text-center">Let s connect</h1>
             <p className="text-center">Be the first to receive exclusives offers</p>
             <form className="form-newsletter-mobile d-flex flex-column align-items-center">
